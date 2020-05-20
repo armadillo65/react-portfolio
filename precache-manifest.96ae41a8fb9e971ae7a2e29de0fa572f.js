ae1aaacf1f2be90c6f568255538d69509afbc38e
Brian Martino <martinowebdesign@gmail.com>
2020-05-19 22:44:33 -0700

    Updates

---
+++ precache-manifest.96ae41a8fb9e971ae7a2e29de0fa572f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10458d92ba83e53b977098cbdf0d5b97",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0eafe09aa8344c757aa4",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6573e5d7866cf1fa9df3",
+    "url": "/react-portfolio/static/js/2.7178584e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.7178584e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eafe09aa8344c757aa4",
+    "url": "/react-portfolio/static/js/main.19128023.chunk.js"
+  },
+  {
+    "revision": "acbdefaa5ce6eb53784a",
+    "url": "/react-portfolio/static/js/runtime-main.2aedd0ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
